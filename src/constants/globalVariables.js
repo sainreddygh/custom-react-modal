25722aba47f8713cc52b200a90cb427ac5e893a7
sainreddygh <nsainath.bits@gmail.com>
2018-12-28 09:48:52 -0800

    code for custom modal

---
+++ src/constants/globalVariables.js
@@ -0,0 +1,17 @@
+export default {
+    globalVariable: {
+      messageText: "Pass your msg here.",
+      resetText: "Cancel",
+      saveText: "Ok",
+      removePreviewFunction: () => {
+        console.log("Write ur Preview remove function");
+      },
+      resetFunction: () => {
+        console.log("Write ur Reset function");
+      },
+      saveFunction: () => {
+        console.log("Write ur Save function");
+      }
+    }
+  };
+  
